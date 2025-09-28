c56ae187cfedcce71e503709025fe250b6179898
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-28 11:15:53 +0530

    Added data_distribution_graph.py

---
+++ data_distribution_graph.py
@@ -0,0 +1,16 @@
+# import numpy
+# x = numpy.random.uniform(0.0, 5.0, 250)
+# print(x)
+
+
+# import numpy
+# import matplotlib.pyplot as plt
+# x = numpy.random.uniform(0.0, 5.0, 100000)
+# plt.hist(x, 100)
+# plt.show()
+
+import numpy
+import matplotlib.pylab as plt
+x = numpy.random.normal(0.0, 5.0, 250)
+plt.hist(x, 5)
+plt.show()
