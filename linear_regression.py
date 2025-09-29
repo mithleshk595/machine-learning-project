9f12939cbd84721e000630773cb3342a68bb84cf
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-29 22:24:49 +0530

    Added linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,24 @@
+# import matplotlib.pylab as plt
+# x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+# y = [99,86,87,88,100,86,103,87,94,78,77,85,86]
+
+# plt.scatter(x ,y)
+# plt.show()
+
+
+
+
+import matplotlib.pyplot as plt
+from scipy import stats
+
+x = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9]
+y = [99, 86, 87, 88, 100, 86, 103, 87, 94, 78, 77, 85]
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+def myfunc(x):
+    return slope * x + intercept
+
+plt.scatter(x, y)
+plt.plot(x, [myfunc(i) for i in x], color="red")  # regression line
+plt.show()
